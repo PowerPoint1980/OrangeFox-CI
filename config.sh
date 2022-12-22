5dd4694ba6ebfc480986a3b2e65ad5387fa456aa
PowerPoint1980 <105037778+PowerPoint1980@users.noreply.github.com>
2022-12-22 20:50:42 +0300

    Update config.sh
    Upgraded bootctl 1.1 to 1.2

--- config.sh
+++ config.sh
@@ -2,7 +2,7 @@
 
 # Device
 export FOX_BRANCH="fox_12.1"
-export DT_LINK="https://github.com/OrangeFoxRecovery/device_xiaomi_mojito.git -b fox_12.1"
+export DT_LINK="https://github.com/PowerPoint1980/device_xiaomi_mojito -b fox_12.1"
 
 export DEVICE="mojito"
 export OEM="xiaomi"
